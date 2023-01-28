f093142051eef177afd74f037a1cd8e5fb190514
NandhakumarRNK <nandha28052000@gmail.com>
2023-01-28 12:50:30 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# SCIENTIFIC CALCULATOR
+### SCIENTIFIC CALCULATOR
+
+- Simple SCIENTIFIC CALCULATOR Design Using HTML CSS & JavaScript
+
+### screenshot
+![screenShot](https://user-images.githubusercontent.com/121867811/215095147-bc3a76b8-59c5-4353-8e97-9cb003e9bc8d.png)
---
+++ js/script.js
@@ -0,0 +1,56 @@
+const actions = document.querySelector('.actions');
+	const ans = document.querySelector('.ans');
+	console.log(actions);
+	console.log(ans);
+	let expression = '';
+	let a=0;
+	actions.addEventListener('click', (e) => {
+		console.log(e.target);
+		const value = e.target.dataset['value'];
+
+		if(value !== undefined) {
+			if(value == 'ce') {
+				expression = '';
+				ans.value = 0;
+				return true;
+			}
+			else if(value == 'x^2'){
+				expression =square();
+			}
+			
+			else if(value == 'radic'){
+				expression = Math.sqrt(expression);
+			}
+			else if(value == 'log'){
+				expression = Math.log(expression);
+			}
+			else if(value == 'sin'){
+				expression = Math.sin(expression);
+			}
+			else if(value == 'cos'){
+				expression = Math.cos(expression);
+			}
+			else if(value == 'tan'){
+				expression = Math.tan(expression);
+			}
+
+			else if(value == '=') {
+				const answer = eval(expression);
+				expression = answer;
+				
+			} else {
+				expression += value;
+			}
+
+			if(expression == undefined) {
+				expression = '';
+				ans.value = 0;
+			} else {
+				ans.value = expression;
+			}
+		}
+
+	});
+	const square =()=> {
+			return eval(expression*expression);
+	}
